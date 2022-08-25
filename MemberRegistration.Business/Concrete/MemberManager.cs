d1acda660623d13fb430de6c01b10b1f128490a7
sezer <sezer.ayran@gmail.com>
2022-08-26 00:48:09 +0300

    Implement Business layer
    Add Core project reference
    Create IMemberService and MemberManager

---
+++ MemberRegistration.Business/Concrete/MemberManager.cs
@@ -0,0 +1,26 @@
+﻿using MemberRegistration.Business.Abstract;
+using MemberRegistration.DataAccess.Abstract;
+using MemberRegistration.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberRegistration.Business.Concrete
+{
+    public class MemberManager : IMemberService
+    {
+        private readonly IMemberDal _memberDal;
+
+        public MemberManager(IMemberDal memberDal)
+        {
+            _memberDal = memberDal;
+        }
+
+        public void Add(Member member)
+        {
+            _memberDal.Add(member);
+        }
+    }
+}
