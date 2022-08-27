0b6f987f11e5a670a9315f10e3b1b2389961fe25
sezer <sezer.ayran@gmail.com>
2022-08-28 00:13:36 +0300

    Install some packages on business layer and create some objects to use in entity validation operations
    some packages = PostSharp v6.10.14 and FluentValidation v8.6.1
    some objects = MemberValidator and FluentValidationAspect

--- MemberRegistration.Business/Concrete/MemberManager.cs
+++ MemberRegistration.Business/Concrete/MemberManager.cs
@@ -1,5 +1,7 @@
-﻿using MemberRegistration.Business.Abstract;
+﻿using DevFramework.Core.Aspects.PostSharp.ValidationAspects;
+using MemberRegistration.Business.Abstract;
 using MemberRegistration.Business.ServiceAdapters.Kps;
+using MemberRegistration.Business.ValidationRules.FluentValidation;
 using MemberRegistration.DataAccess.Abstract;
 using MemberRegistration.Entities.Concrete;
 using System;
@@ -21,6 +23,7 @@ namespace MemberRegistration.Business.Concrete
             _kpsService = kpsService;
         }
 
+        [FluentValidationAspect(typeof(MemberValidator))]
         public void Add(Member member)
         {
             if (_kpsService.Validate(member) == false)
---
+++ MemberRegistration.Business/ValidationRules/FluentValidation/MemberValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using MemberRegistration.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberRegistration.Business.ValidationRules.FluentValidation
+{
+    public class MemberValidator : AbstractValidator<Member>
+    {
+        public MemberValidator()
+        {
+            RuleFor(m => m.TcNo).NotEmpty().Length(11);
+            RuleFor(m => m.Email).NotEmpty().EmailAddress();
+            RuleFor(m => m.DateOfBirth).NotEmpty().LessThan(DateTime.Now);
+            RuleFor(m => m.FirstNane).NotEmpty();
+            RuleFor(m => m.LastName).NotEmpty();
+        }
+    }
+}
