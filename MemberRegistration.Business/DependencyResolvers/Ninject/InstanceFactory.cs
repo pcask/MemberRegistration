4b3ac0d3319156d50a76936f16b92d5e293957f3
sezer <sezer.ayran@gmail.com>
2022-08-28 10:09:07 +0300

    Create a factory to instantiate any object we need
    InstanceFactory.cs

---
+++ MemberRegistration.Business/DependencyResolvers/Ninject/InstanceFactory.cs
@@ -0,0 +1,19 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberRegistration.Business.DependencyResolvers.Ninject
+{
+    // Generic instance üretimi ihtiyacımızı IoC container kullanarak karşılayacak sınıfımız;
+    public class InstanceFactory
+    {
+        public static T GetInstance<T>()
+        {
+            var kernel = new StandardKernel(new BusinessModule());
+            return kernel.Get<T>();
+        }
+    }
+}
